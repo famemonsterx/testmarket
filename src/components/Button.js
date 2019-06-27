0ebe402529fd78fd2acbe6dabde7979698ebbb34
Daniil Ustiantsev <zesofa@gmail.com>
2019-06-27 15:18:30 +0300

    add plus-minus buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Button = (props) => {
-    return (
-        <button className="btn-orange" data-product-id={props.post.productId}>в корзину</button>
-    )
+class Button extends Component {
+    constructor (props) {
+        super(props)
+        this.plusValue = this.plusValue.bind(this)
+        this.minusValue = this.minusValue.bind(this)
+        this.state = {
+            value: 1
+        }
+    }
+    plusValue (event) {
+        event.preventDefault()
+        const {value} = this.state
+        this.setState({
+            value: value+1
+        })
+    }
+    minusValue(event) {
+        event.preventDefault()
+        const {value} = this.state
+        this.setState({
+            value: value === 1 ? value : value-1
+        })
+    }
+    render () {
+        const {post} = this.props
+        const {value} = this.state
+        return (
+            <div className="buttonsGroup">
+                <span className="default-counter">{ value }</span>
+                <button className="add-button" onClick={this.plusValue} >+</button>
+                <button className="remove-button" onClick={this.minusValue}>-</button>
+                <button className="btn-orange" data-product-id={post.productId}>в корзину</button>
+            </div>
+
+        )
+    }
 }
 
 export default Button
