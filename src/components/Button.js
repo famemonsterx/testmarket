9948865d9bd1af92d711439c154ebed11c62f0b9
Daniil Ustiantsev <zesofa@gmail.com>
2019-06-27 12:51:01 +0300

    add function component

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProductData from '../data/products'
+
+const Button = () => {
+    return (
+        <button className="btn-orange" data-product-id=""></button>
+    )
+}
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,7 +5,13 @@ const ProductList = () => {
     return (
         <div>
             {ProductData.map((postDetail, index)=>{
-                return <h1>{postDetail.title}</h1>
+                return (
+                    <div>
+                        <h1>{postDetail.title}</h1>
+                        <img width="200px" height="200px" src={postDetail.primaryImageUrl} />
+                        <button data-product-id={postDetail.productId}></button>
+                    </div>
+                )
             })}
         </div>
     )
